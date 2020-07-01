4670ba3dd21082e506db22d147a7c417e038d08a
viperior <viperior@gmail.com>
2020-07-01 20:37:16 +0000

    Create feature that parses the HTML of a wiki topic article and finds outbound wiki links and topics.

---
+++ download_wiki_topic_page.py
@@ -0,0 +1,20 @@
+import requests
+
+def download_wiki_topic_page(topic):
+    wiki_article_url = 'https://en.wikipedia.org/wiki/' + topic.replace(' ', '_')
+    target_content_file_path = 'data/article-' + topic.lower().replace(' ', '-') + '.html'
+    print('Retrieving data on topic, ' + topic.lower() + ' from ' + wiki_article_url + '...')
+    r = requests.get(wiki_article_url)
+    response_code = r.status_code
+    
+    if response_code == 200:
+        print('Encoding = ' + r.encoding)
+        response_text = r.text
+        print('Saving topic data to file: ' + target_content_file_path + '...')
+        
+        with open(target_content_file_path, 'w') as target_content_file:
+            target_content_file.write(response_text)
+    else:
+        print('Error occurred while trying to retrieve the page.')
+        print('URL: ' + wiki_article_url)
+        print('Response code: ' + response_code)
