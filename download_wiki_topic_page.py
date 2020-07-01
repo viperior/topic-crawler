2528497550f5474920e91dc6043c0155fb20a509
viperior <viperior@gmail.com>
2020-07-01 20:43:30 +0000

    Add check to determine if wiki article has been previously downloaded.

--- download_wiki_topic_page.py
+++ download_wiki_topic_page.py
@@ -1,20 +1,26 @@
+import os
 import requests
 
 def download_wiki_topic_page(topic):
-    wiki_article_url = 'https://en.wikipedia.org/wiki/' + topic.replace(' ', '_')
     target_content_file_path = 'data/article-' + topic.lower().replace(' ', '-') + '.html'
-    print('Retrieving data on topic, ' + topic.lower() + ' from ' + wiki_article_url + '...')
-    r = requests.get(wiki_article_url)
-    response_code = r.status_code
+    wiki_article_url = 'https://en.wikipedia.org/wiki/' + topic.replace(' ', '_')
     
-    if response_code == 200:
-        print('Encoding = ' + r.encoding)
-        response_text = r.text
-        print('Saving topic data to file: ' + target_content_file_path + '...')
-        
-        with open(target_content_file_path, 'w') as target_content_file:
-            target_content_file.write(response_text)
+    if os.path.isfile('target_content_file_path'):
+        print('Skipping download of wiki article. Copy already exists...')
+        print('Skipped: ' + wiki_article_url)
     else:
-        print('Error occurred while trying to retrieve the page.')
-        print('URL: ' + wiki_article_url)
-        print('Response code: ' + response_code)
+        print('Retrieving data on topic, ' + topic.lower() + ', from ' + wiki_article_url + '...')
+        r = requests.get(wiki_article_url)
+        response_code = r.status_code
+        
+        if response_code == 200:
+            print('Encoding = ' + r.encoding)
+            response_text = r.text
+            print('Saving topic data to file: ' + target_content_file_path + '...')
+            
+            with open(target_content_file_path, 'w') as target_content_file:
+                target_content_file.write(response_text)
+        else:
+            print('Error occurred while trying to retrieve the page.')
+            print('URL: ' + wiki_article_url)
+            print('Response code: ' + response_code)
