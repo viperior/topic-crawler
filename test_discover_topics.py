b8be26a85003fa15db92ec3fb43916c06a8539cf
viperior <viperior@gmail.com>
2020-07-02 00:16:06 +0000

    Expand restrictions of topical discovery test script.

--- test_discover_topics.py
+++ test_discover_topics.py
@@ -6,6 +6,6 @@ discover_topics.discover_topics(
     topic_wiki_article_relative_url='/wiki/Nuclear_fusion',
     topic_article_file_path='data/article-' + topic.lower().replace(' ', '-') + '.html',
     topic=topic,
-    crawl_distance=2,
-    crawl_limit_per_node=3
+    crawl_distance=3,
+    crawl_limit_per_node=20
 )
