6ec89c3e9b20f1f9feb3f78799df8396e638b14d
viperior <viperior@gmail.com>
2020-07-02 17:04:34 +0000

    Update test script to use new function parameters.

--- test_discover_topics_information_engineering.py
+++ test_discover_topics_information_engineering.py
@@ -1,12 +1,8 @@
 import discover_topics
 
-topic = 'information engineering'
-
 discover_topics.discover_topics(
-    topic_wiki_article_relative_url='/wiki/Information_engineering',
-    topic_article_file_path='data/article-' + topic.lower().replace(' ', '-') + '.html',
-    topic=topic,
+    topic_slug='Information_engineering',
     crawl_distance=3,
     crawl_limit_per_node=20,
-    verbose=True
+    verbose=False
 )
