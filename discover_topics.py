1db677368680ce54f4b8a281095736a67186b46c
viperior <viperior@gmail.com>
2020-07-02 14:35:03 +0000

    Change breadcrumbs to a list and improve documentation.

--- discover_topics.py
+++ discover_topics.py
@@ -9,40 +9,58 @@ def discover_topics(topic_wiki_article_relative_url, topic_article_file_path,
     """Discover topics related to the starting topic.
 
     Keyword arguments:
-    starting_topic -- the topic of origin for the crawl session
+    topic_wiki_article_relative_url -- the relative URL of the current topic's
+        wiki page
+    topic_article_file_path -- the relative path the current topics downloaded
+        wiki article file
+    topic -- the current topic
     crawl_distance -- the max distance to crawl from the origin (default 3)
-    crawl_limit_per_node -- the max number of linked topics to crawl per node (default 10)
+    crawl_limit_per_node -- the max number of linked topics to crawl per node 
+        (default 10)
+    parent_breadcrumbs -- the breadcrumbs of the parent node, if the current
+        function call is a lower tree node (default None)
+    verbose -- displays information about the current crawl session if set to
+        True (default False)
     """
     
+    # Create breadcrumbs.
     if parent_breadcrumbs == None:
-        current_breadcrumbs = topic
+        current_breadcrumbs = [topic]
     else:
-        current_breadcrumbs = parent_breadcrumbs + ' > ' + topic
-        
+        current_breadcrumbs = parent_breadcrumbs
+        current_breadcrumbs.append(topic)
+    
+    # Display breadcrumbs.
     if verbose:
-        print(current_breadcrumbs)
-        
+        print(' > '.join(current_breadcrumbs))
+    
+    # Download the current topic's wiki page.
     download_wiki_topic_page.download_wiki_topic_page(
         topic_wiki_article_relative_url=topic_wiki_article_relative_url,
         topic=topic
     )
+    
+    # Extract links from the current topic's wiki page.
     extract_outbound_links.extract_outbound_links(
         topic_article_file_path=topic_article_file_path,
         topic=topic
     )
     
+    # Load the current topic's outbound link data.
     outbound_links_file_path = 'data/outbound_links_' + \
         topic.replace(' ', '_').lower() + '.json'
     
     with open(outbound_links_file_path, 'r') as outbound_links_file:
         outbound_links_original = json.load(outbound_links_file)
-        
+    
+    # Limit number of outbound links to crawl based on set limit.
     outbound_links = {}
         
     for link in outbound_links_original:
         if outbound_links_original[link]['order_index'] + 1 <= crawl_limit_per_node:
             outbound_links[link] = outbound_links_original[link]
-            
+    
+    # Crawl each outbound link.
     for link in outbound_links:
         if crawl_distance > 0:
             discover_topics(
