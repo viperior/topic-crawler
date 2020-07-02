5148e5820ad632c0c671f4809e19d67ceaee0b34
viperior <viperior@gmail.com>
2020-07-02 14:06:10 +0000

    Add test script that discovers topics related to information engineering, aka data engineering.

---
+++ test_discover_topics_information_engineering.py
@@ -0,0 +1,11 @@
+import discover_topics
+
+topic = 'information engineering'
+
+discover_topics.discover_topics(
+    topic_wiki_article_relative_url='/wiki/Information_engineering',
+    topic_article_file_path='data/article-' + topic.lower().replace(' ', '-') + '.html',
+    topic=topic,
+    crawl_distance=3,
+    crawl_limit_per_node=20
+)
