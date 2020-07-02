68ce5d9837fb0df3c21967b87956b1ce1c942270
viperior <viperior@gmail.com>
2020-07-02 15:50:47 +0000

    Update test scripts to use only topic slug.

--- test_download_wiki_topic_page.py
+++ test_download_wiki_topic_page.py
@@ -1,6 +1,5 @@
 import download_wiki_topic_page
 
 download_wiki_topic_page.download_wiki_topic_page(
-    topic_wiki_article_relative_url='/wiki/Nuclear_fusion',
-    topic='Nuclear fusion'
+    topic_slug='Information_engineering'
 )
--- test_extract_outbound_links.py
+++ test_extract_outbound_links.py
@@ -1,6 +1,5 @@
 import extract_outbound_links
 
 extract_outbound_links.extract_outbound_links(
-    topic_article_file_path='data/article-nuclear-fusion.html',
-    topic='nuclear fusion'
+    topic_slug='Information_engineering'
 )
