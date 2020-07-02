95aae5d3333bc63e3fcf0cbbf182ed4415bf1e4f
viperior <viperior@gmail.com>
2020-07-02 14:35:32 +0000

    Set verbose setting to true.

--- test_discover_topics_information_engineering.py
+++ test_discover_topics_information_engineering.py
@@ -7,5 +7,6 @@ discover_topics.discover_topics(
     topic_article_file_path='data/article-' + topic.lower().replace(' ', '-') + '.html',
     topic=topic,
     crawl_distance=3,
-    crawl_limit_per_node=20
+    crawl_limit_per_node=20,
+    verbose=True
 )
