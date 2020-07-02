9bed5efa663a8a2ff8f1b7a078d2b4dbfd4d9f9e
viperior <viperior@gmail.com>
2020-07-02 17:14:58 +0000

    Update test scripts.

--- test_discover_topics_nuclear_fusion.py
+++ test_discover_topics_nuclear_fusion.py
@@ -1,11 +1,8 @@
 import discover_topics
 
-topic = 'nuclear fusion'
-
 discover_topics.discover_topics(
-    topic_wiki_article_relative_url='/wiki/Nuclear_fusion',
-    topic_article_file_path='data/article-' + topic.lower().replace(' ', '-') + '.html',
-    topic=topic,
+    topic_slug='Nuclear_fusion',
     crawl_distance=3,
-    crawl_limit_per_node=20
+    crawl_limit_per_node=4,
+    verbose=False
 )
